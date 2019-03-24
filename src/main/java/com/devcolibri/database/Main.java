336df79f344d3e81299809c47c01b1700d1cac5b
marat <gumerovm74@gmail.com>
2019-03-24 23:58:39 +0300

    выгрузка проекта

---
+++ src/main/java/com/devcolibri/database/Main.java
@@ -0,0 +1,37 @@
+package com.devcolibri.database;
+
+import com.mysql.fabric.jdbc.FabricMySQLDriver;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/my_db_test";
+    private static  final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    public static void main(String[] args) {
+        Connection connection;
+
+
+        try {
+            Driver driver = new FabricMySQLDriver();
+            DriverManager.registerDriver(driver);
+
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            if (!connection.isClosed()){
+                System.out.println("соединение с базой данных установлено");
+            }
+            connection.close();
+            if (connection.isClosed()){
+                System.out.println(" Соединение с базой данных закрыто");
+            }
+
+        } catch (SQLException e) {
+            System.err.println("неудалось загрузить класс драйвера!");
+        }
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: User
+  Date: 15.03.2019
+  Time: 21:02
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title> WEB PROJECT</title>
+  </head>
+  <body>
+   This  is  body.
+  </body>
+</html>
