ce165a5ddd7fec299abd5752c09e4d294171d3b8
marat <gumerovm74@gmail.com>
2019-03-25 00:08:15 +0300

    выгрузка проекта

--- src/main/java/com/devcolibri/database/Main.java
+++ src/main/java/com/devcolibri/database/Main.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 
 public class Main {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/my_db_test";
+     private static final String URL = "jdbc:mysql://localhost:3306/my_db_test";
     private static  final String USERNAME = "root";
     private static final String PASSWORD = "root";
 
